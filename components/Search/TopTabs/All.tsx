41bf71086db5cadc5cabb6f11d10bfed0dc9b0ad
Sabin <sabinonweb7@gmail.com>
2023-08-01 07:28:58 +0545

    Search is made

--- components/Search/TopTabs/All.tsx
+++ components/Search/TopTabs/All.tsx
@@ -1,8 +1,9 @@
-import { View, FlatList, Text, Image } from "react-native";
+import { View, FlatList, ScrollView, Text, Image } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const All = () => {
-  const Data = [
+  const DataLeft = [
     {
       name: "problemas en el paraiso",
       backgroundColor: "#dbaf8a",
@@ -40,89 +41,289 @@ const All = () => {
       price: "$40",
     },
     {
+      name: "Muchas graciss",
+      backgroundColor: "#f5e5cb",
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      image: require("../../../assets/Food/12.png"),
+      key: 5,
+      price: "$40",
+    },
+    {
+      name: "problemas en el paraiso",
+      backgroundColor: "#dbaf8a",
+      paddingHorizontal: 50,
+      paddingVertical: 50,
+      image: require("../../../assets/Food/14.png"),
+      key: 6,
+      price: "$40",
+    },
+  ];
+
+  const DataRight = [
+    {
       name: "Parece que fue ayer, ¿no?",
       backgroundColor: "#f0b9d3",
       paddingHorizontal: 10,
-      paddingVertical: 90,
+      paddingVertical: 10,
       image: require("../../../assets/Food/17.png"),
-      key: 5,
+      key: 7,
       price: "$40",
     },
     {
       name: "Qué calor / frío hace hoy! ",
       backgroundColor: "#cbecf5",
       paddingHorizontal: 10,
-      paddingVertical: 90,
+      paddingVertical: 120,
       image: require("../../../assets/Food/16.png"),
-      key: 6,
+      key: 8,
       price: "$40",
     },
     {
       name: "Hala Madrid",
       backgroundColor: "#b9d5f0",
-      paddingHorizontal: 90,
-      paddingVertical: 90,
+      paddingHorizontal: 50,
+      paddingVertical: 50,
       image: require("../../../assets/Food/15.png"),
-      key: 7,
+      key: 9,
       price: "$40",
     },
     {
       name: "No me vengas con ",
-      backgroundColor: "#b9f0c0",
+      backgroundColor: "#f7e1e8",
       paddingHorizontal: 90,
       paddingVertical: 90,
       image: require("../../../assets/Food/18.png"),
-      key: 8,
+      key: 10,
+      price: "$40",
+    },
+    {
+      name: "Siuuuuuuuuuu",
+      backgroundColor: "#67b5ac",
+      paddingHorizontal: 90,
+      paddingVertical: 90,
+      image: require("../../../assets/Food/15.png"),
+      key: 11,
+      price: "$40",
+    },
+    {
+      name: "Bienvenido",
+      backgroundColor: "#b56767",
+      paddingHorizontal: 70,
+      paddingVertical: 70,
+      image: require("../../../assets/Food/13.png"),
+      key: 12,
       price: "$40",
     },
   ];
 
-  const renderItem = ({ item, index }) => (
-    <View style={{ flex: 1 }} key={item.key}>
-      <View
-        style={{
-          borderWidth: 0.1,
-          paddingHorizontal: item.paddingHorizontal,
-          paddingVertical: item.paddingVertical,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: item.backgroundColor,
-          marginLeft: 20,
-          marginRight: 20,
-          borderRadius: 30,
-          marginTop: 30,
-        }}
-      >
-        <Image source={item.image} style={{ height: 100, width: 100 }} />
-        <Text>{item.price}</Text>
-      </View>
-      <View
-        style={{
-          flexWrap: "wrap",
-          alignItems: "flex-start",
-          justifyContent: "flex-start",
-          marginLeft: 30,
-          marginTop: 20,
-        }}
-      >
-        <Text
-          style={{ alignSelf: "flex-start", fontSize: 18, fontWeight: 700 }}
-          numberOfLines={2}
-        >
-          {item.name}
-        </Text>
-      </View>
-    </View>
-  );
+  const Length = DataLeft.length;
+  const Rength = DataRight.length;
 
   return (
-    <FlatList
-      data={Data}
-      numColumns={2}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.key.toString()}
-    ></FlatList>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={{ flexDirection: "row" }}>
+        <View style={{ flex: 0.5 }}>
+          {DataLeft.map((item, index) => {
+            if (index <= Length) {
+              return (
+                <View style={{ flex: 1 }} key={item.key}>
+                  <View
+                    style={{
+                      borderWidth: 0.1,
+                      paddingHorizontal: item.paddingHorizontal,
+                      paddingVertical: item.paddingVertical,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: item.backgroundColor,
+                      marginLeft: 20,
+                      marginRight: 20,
+                      borderRadius: 30,
+                      marginTop: 30,
+                    }}
+                  >
+                    <Image
+                      source={item.image}
+                      style={{ height: 100, width: 100 }}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      flexWrap: "wrap",
+                      alignItems: "flex-start",
+                      justifyContent: "flex-start",
+                      marginLeft: 30,
+                      marginTop: 20,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        alignSelf: "flex-start",
+                        fontSize: 18,
+                        fontWeight: 700,
+                      }}
+                      numberOfLines={2}
+                    >
+                      {item.name}
+                    </Text>
+                    <View
+                      style={{
+                        flex: 1,
+                        justifyContent: "flex-start",
+                        flexDirection: "row",
+                      }}
+                    >
+                      <Ionicons name="cash-outline" size={24} />
+                      <Text
+                        style={{
+                          paddingLeft: 10,
+                          paddingTop: 1,
+                          fontSize: 18,
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {item.price}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </View>
+        <View style={{ flex: 0.5 }}>
+          {DataRight.map((item, index) => {
+            if (index <= Rength) {
+              return (
+                <View style={{ flex: 1 }} key={item.key}>
+                  <View
+                    style={{
+                      borderWidth: 0.1,
+                      paddingHorizontal: item.paddingHorizontal,
+                      paddingVertical: item.paddingVertical,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: item.backgroundColor,
+                      marginLeft: 20,
+                      marginRight: 20,
+                      borderRadius: 30,
+                      marginTop: 30,
+                    }}
+                  >
+                    <Image
+                      source={item.image}
+                      style={{ height: 100, width: 100 }}
+                    />
+                    <Text>{item.price}</Text>
+                  </View>
+                  <View
+                    style={{
+                      flexWrap: "wrap",
+                      alignItems: "flex-start",
+                      justifyContent: "flex-start",
+                      marginLeft: 30,
+                      marginTop: 20,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        alignSelf: "flex-start",
+                        fontSize: 18,
+                        fontWeight: 700,
+                      }}
+                      numberOfLines={2}
+                    >
+                      {item.name}
+                    </Text>
+                    <View
+                      style={{
+                        flex: 1,
+                        justifyContent: "flex-start",
+                        flexDirection: "row",
+                      }}
+                    >
+                      <Ionicons name="cash-outline" size={24} />
+                      <Text
+                        style={{
+                          paddingLeft: 10,
+                          paddingTop: 1,
+                          fontSize: 18,
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {item.price}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default All;
+
+// const renderItem = ({ item, index }) => (
+//   <View style={{ flex: 1 }} key={item.key}>
+//     <View
+//       style={{
+//         borderWidth: 0.1,
+//         paddingHorizontal: item.paddingHorizontal,
+//         paddingVertical: item.paddingVertical,
+//         justifyContent: "center",
+//         alignItems: "center",
+//         backgroundColor: item.backgroundColor,
+//         marginLeft: 20,
+//         marginRight: 20,
+//         borderRadius: 30,
+//         marginTop: 30,
+//       }}
+//     >
+//       <Image source={item.image} style={{ height: 100, width: 100 }} />
+//       <Text>{item.price}</Text>
+//     </View>
+//     <View
+//       style={{
+//         flexWrap: "wrap",
+//         alignItems: "flex-start",
+//         justifyContent: "flex-start",
+//         marginLeft: 30,
+//         marginTop: 20,
+//       }}
+//     >
+//       <Text
+//         style={{ alignSelf: "flex-start", fontSize: 18, fontWeight: 700 }}
+//         numberOfLines={2}
+//       >
+//         {item.name}
+//       </Text>
+//     </View>
+//   </View>
+// );
+
+// return (
+//   <View style={{ flex: 1, flexDirection: "row" }}>
+//     <View style={{ flex: 0.5 }}>
+//       <FlatList
+//         data={Data}
+//         renderItem={renderItem}
+//         keyExtractor={(item) => item.key.toString()}
+//       ></FlatList>
+//     </View>
+//     <View style={{ flex: 0.5 }}>
+//       <FlatList
+//         data={Data}
+//         renderItem={renderItem}
+//         keyExtractor={(item) => item.key.toString()}
+//       ></FlatList>
+//     </View>
+//   </View>
+// );
